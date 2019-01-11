b96c9edc6295615f8217c3eccaebc7dc0bc53b70
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-11 04:47:33 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,19 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
 
+  # def category_ids= ids
+  #   ids.each do |i|
+  #     # category = Category.find(i) unless i.empty?
+  #     self.categories << Category.find(i) unless i.empty?
+  #   end
+  # end
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.post_categories.build(category: category)
+    category_attributes.values.each do |category_attribute| #name: 'val'
+      # category = Category.find_or_create_by(category_attribute) unless category_attribute.empty?
+      # self.post_categories.build(category: category)
+      # binding.pry
+       self.categories << Category.find_or_create_by(category_attribute) unless category_attribute.values[0].empty?
     end
   end
 
